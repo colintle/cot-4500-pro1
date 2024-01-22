8364910b7ea8be30ee7a0b32ae443c4c02b8fcb9
Colin Le <Colin.Le@ucf.edu>
2024-01-22 13:15:53 -0500

    removed unnecessary code

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -1,5 +1,3 @@
-import math
-
 def bisection_method(f, a, b, tol, max_iter=1000):
     i = 0
     left = a
@@ -36,12 +34,16 @@ def newton_raphson_method(f, df, x0, tol, max_iter=50):
     for i in range(max_iter):
         f_val = f(x0)
         df_val = df(x0)
+
         if df_val == 0:
             return None, i, "FAILURE: Derivative is zero"
+        
         x1 = x0 - f_val / df_val
+
         if abs(x1 - x0) < tol:
             return x1, i + 1, "SUCCESS"
         x0 = x1
+
     return x0, max_iter, "FAILURE: Max iterations reached"
 
 def approximation_algorithm(value, tol=0.00001):
