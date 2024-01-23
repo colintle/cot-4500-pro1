8f1f947958d1349a9c6305e2de62471f07efae07
Colin Le <Colin.Le@ucf.edu>
2024-01-22 19:56:33 -0500

    Modified tolerance

--- src/test/test_assignment_1.py
+++ src/test/test_assignment_1.py
@@ -9,7 +9,7 @@ print(f"Approximated square root of {number_to_find_root_of}: {approximated_root
 def function(x):
     return (x**3) + 4*(x**2) - 10
 
-root, iterations = bisection_method(function, a=1, b=2, tol=1e-5)
+root, iterations = bisection_method(function, a=1, b=2, tol=1e-3)
 print("Bisection Method")
 print(f"The root found is: {root}")
 print("The number of iterations bisection method took is " + str(iterations))
@@ -22,7 +22,7 @@ def g2(x):
     return (10 - x**3)**0.5 / 2
 
 initial_guess = 1.5
-tolerance = 0.00001
+tolerance = 0.000001
 
 print("Fixed-Point Iteration")
 root_a, iterations_a, status_a = fixed_point_iteration(g1, initial_guess, tolerance)
@@ -39,7 +39,7 @@ def df(x):
     return -math.sin(x) - 1
 
 initial_guess = math.pi / 4
-tolerance = 1e-5
+tolerance = 1e-6
 
 print("Newton-Raphson Method")
 root, iterations, status = newton_raphson_method(f, df, initial_guess, tolerance)
